1ee0ce54714029a0ace61b69a5c7b15ad6af4409
Johannes Mols <johannes.mols@googlemail.com>
2020-03-21 22:47:08 +0100

    Added gitignore and config file

---
+++ config.py
@@ -0,0 +1,13 @@
+rpc = {
+    'endpoint': '127.0.0.1:8332',
+    'rpcuser': 'johannes',
+    'rpcpassword': 'nicepassword'
+}
+
+db = {
+    'server': '',
+    'database': 'bitcoin',
+    'username': 'postgres',
+    'password': '',
+    'port': '5432'
+}
